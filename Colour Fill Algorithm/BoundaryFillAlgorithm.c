baa85339d95476dbe68de5c430ca34356ef29c29
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2025-08-08 20:16:41 +0530

    Boundary Fill Algorithm added

--- Colour Fill Algorithm/BoundaryFillAlgorithm.c
+++ Colour Fill Algorithm/BoundaryFillAlgorithm.c
@@ -4,62 +4,35 @@
 #include <math.h>
 #include <stdlib.h>
 
+int xc, yc, radius, bordercolorRed, bordercolorGreen, bordercolorBlue, fillcolorRed, fillcolorGreen, fillcolorBlue;
+
 void DrawCirclePoints(HDC hdc, int xc, int yc, int x, int y, COLORREF bordercolor)
 {
-    printf("Current Color at (%d, %d): R=%d, G=%d, B=%d\n", x, y,
-           GetRValue(bordercolor),
-           GetGValue(bordercolor),
-           GetBValue(bordercolor));
-    printf("pixl colored : (%d,%d)\n", xc + x, yc + y);
     SetPixel(hdc, xc + x, yc + y, bordercolor);
-    printf("pixl colored : (%d,%d)\n", xc - x, yc + y);
     SetPixel(hdc, xc - x, yc + y, bordercolor);
-    printf("pixl colored : (%d,%d)\n", xc + x, yc - y);
     SetPixel(hdc, xc + x, yc - y, bordercolor);
-    printf("pixl colored : (%d,%d)\n", xc - x, yc - y);
     SetPixel(hdc, xc - x, yc - y, bordercolor);
-    printf("pixl colored : (%d,%d)\n", xc + y, yc + x);
     SetPixel(hdc, xc + y, yc + x, bordercolor);
-    printf("pixl colored : (%d,%d)\n", xc - y, yc + x);
     SetPixel(hdc, xc - y, yc + x, bordercolor);
-    printf("pixl colored : (%d,%d)\n", xc + y, yc - x);
     SetPixel(hdc, xc + y, yc - x, bordercolor);
-    printf("pixl colored : (%d,%d)\n", xc - y, yc - x);
     SetPixel(hdc, xc - y, yc - x, bordercolor);
 }
 
-void boundaryFillN4(HDC hdc, int x, int y, int x1, int x2, int y1, int y2, COLORREF bordercolor, COLORREF fillColor)
+void boundaryFillN4(HDC hdc, int x, int y, COLORREF bordercolor, COLORREF fillColor)
 {
-    printf("Pixel no : (%d, %d)\n", x, y);
     COLORREF currentColor = GetPixel(hdc, x, y);
 
-    printf("Current Color at (%d, %d): R=%d, G=%d, B=%d\n", x, y,
-           GetRValue(currentColor),
-           GetGValue(currentColor),
-           GetBValue(currentColor));
-    int inp = 0;
-    printf("enter something to proceed : ");
-    scanf("%d", &inp);
-    if (inp == 0)
-    {
-        exit(0);
-    }
     if (currentColor == bordercolor || currentColor == fillColor)
     {
-        // exit(0);
         return;
     }
-    // if (currentColor == bordercolor || currentColor == fillColor || x > x1 || x < x2 || y > y1 || y < y2)
-    // {
-    //     return;
-    // }
     else
     {
         SetPixel(hdc, x, y, fillColor);
-        boundaryFillN4(hdc, x, y + 1, x1, x2, y1, y2, bordercolor, fillColor);
-        boundaryFillN4(hdc, x, y - 1, x1, x2, y1, y2, bordercolor, fillColor);
-        boundaryFillN4(hdc, x + 1, y, x1, x2, y1, y2, bordercolor, fillColor);
-        boundaryFillN4(hdc, x - 1, y, x1, x2, y1, y2, bordercolor, fillColor);
+        boundaryFillN4(hdc, x, y + 1, bordercolor, fillColor);
+        boundaryFillN4(hdc, x, y - 1, bordercolor, fillColor);
+        boundaryFillN4(hdc, x + 1, y, bordercolor, fillColor);
+        boundaryFillN4(hdc, x - 1, y, bordercolor, fillColor);
     }
 }
 
@@ -70,7 +43,6 @@ void MidpointCircleDrawingAlgorithm(HDC hdc, int xc, int yc, int radius, int bor
     xk = 0;
     yk = radius;
 
-    // SetPixel(hdc, xk + xc, yk + yc, RGB(255, 0, 0));
     DrawCirclePoints(hdc, xc, yc, xk, yk, RGB(bordercolorRed, bordercolorGreen, bordercolorBlue));
 
     printf("%7d|%7d|%7d|%7d|%7d|%7d\n", 0, 0, xk, yk, xk + xc, yk + yc);
@@ -84,14 +56,12 @@ void MidpointCircleDrawingAlgorithm(HDC hdc, int xc, int yc, int radius, int bor
             if (pk < 0)
             {
                 xk = xk + 1;
-                // SetPixel(hdc, xk + xc, yk + yc, RGB(255, 0, 0));
                 DrawCirclePoints(hdc, xc, yc, xk, yk, RGB(bordercolorRed, bordercolorGreen, bordercolorBlue));
             }
             else
             {
                 xk = xk + 1;
                 yk = yk - 1;
-                // SetPixel(hdc, xk + xc, yk + yc, RGB(255, 0, 0));
                 DrawCirclePoints(hdc, xc, yc, xk, yk, RGB(bordercolorRed, bordercolorGreen, bordercolorBlue));
             }
             printf("%7d|%7d|%7d|%7d|%7d|%7d\n", k, pk, xk, yk, xk + xc, yk + yc);
@@ -112,28 +82,60 @@ void MidpointCircleDrawingAlgorithm(HDC hdc, int xc, int yc, int radius, int bor
             if (pk < 0)
             {
                 xk = xk + 1;
-                // SetPixel(hdc, xk + xc, yk + yc, RGB(255, 0, 0));
                 DrawCirclePoints(hdc, xc, yc, xk, yk, RGB(bordercolorRed, bordercolorGreen, bordercolorBlue));
             }
             else
             {
                 xk = xk + 1;
                 yk = yk - 1;
-                // SetPixel(hdc, xk + xc, yk + yc, RGB(255, 0, 0));
                 DrawCirclePoints(hdc, xc, yc, xk, yk, RGB(bordercolorRed, bordercolorGreen, bordercolorBlue));
             }
             printf("%7d|%7d|%7d|%7d|%7d|%7d\n", k, pk, xk, yk, xk + xc, yk + yc);
             k++;
         }
     }
+}
 
-    boundaryFillN4(hdc, xc, yc, xc + radius, xc - radius, yc + radius, yc - radius, RGB(bordercolorRed, bordercolorGreen, bordercolorBlue), RGB(fillcolorRed, fillcolorGreen, fillcolorBlue));
+void DrawScene(HWND hwnd)
+{
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hwnd, &ps);
+
+    RECT rect;
+    GetClientRect(hwnd, &rect);
+    int width = rect.right;
+    int height = rect.bottom;
+
+    // Create compatible memory DC
+    HDC memDC = CreateCompatibleDC(hdc);
+    HBITMAP memBitmap = CreateCompatibleBitmap(hdc, width, height);
+    SelectObject(memDC, memBitmap);
+
+    // Fill background white
+    HBRUSH whiteBrush = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    FillRect(memDC, &rect, whiteBrush);
+
+    MidpointCircleDrawingAlgorithm(memDC, xc, yc, radius, bordercolorRed, bordercolorGreen, bordercolorBlue, fillcolorRed, fillcolorGreen, fillcolorBlue);
+
+    // Call boundary fill AFTER drawing
+    boundaryFillN4(memDC, xc, yc, RGB(bordercolorRed, bordercolorGreen, bordercolorBlue), RGB(fillcolorRed, fillcolorGreen, fillcolorBlue));
+
+    // Blit memory DC to screen
+    BitBlt(hdc, 0, 0, width, height, memDC, 0, 0, SRCCOPY);
+
+    // Cleanup
+    DeleteObject(memBitmap);
+    DeleteDC(memDC);
+    EndPaint(hwnd, &ps);
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
     {
+    case WM_PAINT:
+        DrawScene(hwnd);
+        return 0;
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
@@ -144,7 +146,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    int xc, yc, radius, bordercolorRed, bordercolorGreen, bordercolorBlue, fillcolorRed, fillcolorGreen, fillcolorBlue;
     printf("Enter center coordinate : ");
     scanf("%d %d", &xc, &yc);
     printf("Enter Radius : ");
@@ -155,32 +156,32 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     printf("Enter fill Color in RGB format (R G B): ");
     scanf("%d %d %d", &fillcolorRed, &fillcolorGreen, &fillcolorBlue);
 
+    const char CLASS_NAME[] = "CircleFillWindow";
+
     WNDCLASS wc = {0};
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
-    wc.lpszClassName = "MyWindowClass";
+    wc.lpszClassName = CLASS_NAME;
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 
-    
     RegisterClass(&wc);
 
-    HWND hwnd = CreateWindow("MyWindowClass", "Mid Point Circle Drawing Algorithm", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1080, 1080, NULL, NULL, hInstance, NULL);
+    HWND hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        "Midpoint Circle with Boundary Fill (Fixed)",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 800,
+        NULL, NULL, hInstance, NULL);
 
     ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
 
-    HDC hdc = GetDC(hwnd);
-    MidpointCircleDrawingAlgorithm(hdc, xc, yc, radius, bordercolorRed, bordercolorGreen, bordercolorBlue, fillcolorRed, fillcolorGreen, fillcolorBlue);
-
-    Sleep(2000);
-    // boundaryFillN4(hdc, xc, yc, xc + radius, xc - radius, yc + radius, yc - radius, RGB(bordercolorRed, bordercolorGreen, bordercolorBlue), RGB(fillcolorRed, fillcolorGreen, fillcolorBlue));
-    ReleaseDC(hwnd, hdc);
-
-    MSG msg;
+    MSG msg = {0};
     while (GetMessage(&msg, NULL, 0, 0))
     {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
-    return (int)msg.wParam;
+    return 0;
 }
